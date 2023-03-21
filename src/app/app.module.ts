6518ca0ae60ba8326de5f18b2973b45ed2504496
mgruppo <99413368+mgruppo@users.noreply.github.com>
2023-03-21 00:49:33 -0300

    se agregan los componentes principales, se agrega menu de BT en el navbar de la izquierda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    HomeComponent,
+    PageWrapperComponent
   ],
   imports: [
     BrowserModule,
